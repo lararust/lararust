bdffa9351e82b37d0eec66716b2c0723929c740b
Vinicius Rech <vinicius.rech.dev@gmail.com>
2025-11-09 14:02:57 -0300

    Add HTTP requests module and overhaul HTTP stack

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,2 +1,4 @@
+pub mod requests;
+pub mod response;
 pub mod router;
 pub mod server;
--- src/http/router.rs
+++ src/http/router.rs
@@ -1,23 +1,5 @@
-use crate::{ctx, view::view};
+use std::sync::Arc;
 
-use axum::{Router, response::Html, routing::get};
+use axum::{http::Request, response::Response};
 
-/// Registers routes
-pub fn routes() -> Router {
-    Router::new()
-        .route("/", get(home))
-        .route("/health", get(|| async { "OK" }))
-}
-
-/// Example route rendering a LaraBlade template
-pub async fn home() -> Html<String> {
-    // Build context using the ctx! macro
-    let ctx = ctx! {
-        "name" => "Vinícius Rech",
-        "admin" => true,
-        "age" => 32,
-        "roles" => vec!["developer", "maintainer"]
-    };
-
-    view("welcome", ctx)
-}
+pub type Handler = Arc<dyn Fn(Request) -> Response + Send + Sync + 'static>;
